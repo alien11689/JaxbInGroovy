f6f1d9b0ca60ebc5c2478bce762c543f51ed1c9b
Durga Rao Manchikanti <manchikanti@users.noreply.github.com>
2017-04-27 16:17:07 -0500

    package-info.java to package-info.groovy (#1)
    Replace package-info.java with package-info.groovy

--- src/main/groovy/com/blogspot/przybyszd/jaxbingroovy/package-info.java
+++
@@ -1,5 +0,0 @@
-@XmlAccessorType(XmlAccessType.FIELD)
-package com.blogspot.przybyszd.jaxbingroovy;
-
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
